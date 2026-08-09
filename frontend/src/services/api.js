08c38d4d51a774cc6a291a74225ffa73119ec0c8
ishant kumar <ishantjnv2025@gmail.com>
2026-08-09 16:51:23 +0530

    api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,10 @@
 import axios from "axios";
 
-const API_URL = "https://eventbook-xosk.onrender.com";
-
- 
-export const backendUrl = apiUrl.replace(/\/api\/?$/, "");
-
 const api = axios.create({
-    baseURL: apiUrl,
+    baseURL: "https://eventbook-xosk.onrender.com/api",
 });
 
-// Automatically attach the JWT token to every request
 api.interceptors.request.use((config) => {
-
     const token = localStorage.getItem("token");
 
     if (token) {
