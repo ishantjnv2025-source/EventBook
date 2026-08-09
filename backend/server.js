c1ac9a6ffca1b1e754a2a30528d0b45ee5ac2053
ishant kumar <ishantjnv2025@gmail.com>
2026-08-09 15:46:27 +0530

    Fixed PORT to 0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -60,6 +60,6 @@ app.use((error, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0",() => {
     console.log(`Server running on Port ${PORT}`);
 });
